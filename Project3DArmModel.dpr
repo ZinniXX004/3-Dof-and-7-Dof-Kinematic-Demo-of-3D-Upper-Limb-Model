26b86c2a8940888f04246049f40e24f2142ef89a
Jemi <marveljemy8@gmail.com>
2025-12-10 22:48:00 +0700

    yes

---
+++ Project3DArmModel.dpr
@@ -0,0 +1,14 @@
+program Project3DArmModel;
+
+uses
+  Vcl.Forms,
+  Unit3DArmModel in 'Unit3DArmModel.pas' {Form1};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  Application.CreateForm(TForm1, Form1);
+  Application.Run;
+end.
